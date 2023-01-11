5b81e7ccefaae495deeb5261bc182de869cfb653
VladPryima <114105594+VladPryima@users.noreply.github.com>
2023-01-11 15:40:10 +0200

    add comments

--- src/App.js
+++ src/App.js
@@ -1,69 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
-  //Перший блок: ЩО конвертуємо
-const [fromCurrency, setFromCurrency] = React.useState("UAH");
-  //Другий блок: У що конвертуємо
+  //Стейт для вибору валюти (1 блок)
 const [toCurrency, setToCurrency] = React.useState("USD");
-
-  //Поле 
+  //Стейт для вибору валюти (2 блок)
+const [fromCurrency, setFromCurrency] = React.useState("UAH");
+  
+//Стейт для інпутів (1-й блок)
+const [toPrice, setToPrice] = React.useState(0);
+  //Стейт для інпутів (2-й блок)
 const [fromPrice, setFromPrice] = React.useState(0);
-const [toPrice, setToPrice] = React.useState(1);
 
-//const [rates, setRates] = React.useState({});
+
+
+//Зберігаємо список валют
 const ratesRef = React.useRef({});
-
+//Завантажуємо JSON і пакуємо в  useRef - ratesRef
 React.useEffect(() => {
   fetch('currency.json')
     .then((res) => res.json())
     .then((json) => {
-      //setRates(json.rates);
-      ratesRef.current = json.rates;
-      onChangeToPrice(1);
+      
+      ratesRef.current = json.rates; //Привласнюємо useRef-у, а саме current, як обьект, JSON з валютами
+      onChangeToPrice(1); //Початкове значення у полі зліва
     });
-}, []);
+}, []); //Залежність у вигляді пустого массива для спрацьовування useEffect усього раз, після рендеру
 
-//Зміна кількості валюти у полі зліва (ЩО)
-const onChangeFromPrice = (value) => {
-  const price = value / ratesRef.current[fromCurrency];
-  const result = price * ratesRef.current[toCurrency];
-  setFromPrice(value);
-  setToPrice(result.toFixed(3));
-}
-//Зміна кількосьті валюти у полі зправа (У ЩО)
+
+//1 Обробник для першого інпута - встановлює значення другого (результат)
 const onChangeToPrice = (value) => {
+ 
   const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
-  setFromPrice(result.toFixed(3));
-  setToPrice(value);
+  setToPrice(value); //Перше поле
+  setFromPrice(result.toFixed(3)); //Друге поле
+  
 }
-
+//useEffect-у передаємо onChangeToPrice з значенням toPrice
 React.useEffect(() => {
-onChangeFromPrice(fromPrice);
-}, [fromCurrency]);
-
-React.useEffect(() => {
+ 
   onChangeToPrice(toPrice);
-  }, [toCurrency]);
+  }, [ toCurrency]); 
 
+//2 Другий
+const onChangeFromPrice = (value) => {
+ 
+  const result = (ratesRef.current[toCurrency] / ratesRef.current[fromCurrency]) * value;
+  setToPrice(result.toFixed(3)); //Перше поле
+  setFromPrice(value); //Друге поле
+  
+}
+//useEffect-у передаємо onChangeToPrice з значенням toPrice
+React.useEffect(() => {
+ 
+  onChangeFromPrice(fromPrice);
+  }, [ fromCurrency]); 
+  
+
+ 
+
+  const buy = {"USD": 1, "EUR": 1, "GBP": 1};
+  const sell = {"USD": 37.37, "EUR": 38.94, "GBP": 44.57};
+
+  const currencyEurope = ["$", "€", "£"];
+  const currencyUa = ["UAH", "UAH", "UAH"];
+
+ const buyOrSell = ["Currency", "Cost (UAH)"];
+
+ /*React.useEffect(() => { //Заборона на ввід для другого інпуту
+  document.querySelectorAll("input")[1].setAttribute("readonly", "readonly");
+ }, []);*/
 
   return (
+    
     <div className="App">
+      <h1>Currency exchange</h1>
       <a className="choosePage" href='https://vladpryima.github.io/home/'>&#129080; До вибору проєктів</a>
-      <Block 
-      value={fromPrice} 
-      currency={fromCurrency} 
-      onChangeCurrency={setFromCurrency} 
-      onChangeValue={onChangeFromPrice}/>
-
       <Block 
       value={toPrice} 
       currency={toCurrency} 
-      onChangeCurrency={setToCurrency} 
-       onChangeValue={onChangeToPrice} />
+      onChangeCurrency={setToCurrency} //Обробник для вибору валюти зі списку
+      onChangeValue={onChangeToPrice} 
+       direction={`GIVE: ${toCurrency} `}
+       obj={buy}
+       buySell={buyOrSell[0]}
+       currencyIcon={currencyEurope}
+       />
+      
+      <Block 
+      value={fromPrice} 
+      currency={fromCurrency} 
+      onChangeCurrency={setFromCurrency} //Обробник для вибору валюти зі списку
+      onChangeValue={onChangeFromPrice} 
+      direction={`GET: ${fromCurrency} `}
+      obj={sell}
+      buySell={buyOrSell[1]}
+      currencyIcon={currencyUa}
+      />
+
+      
     </div>
   );
 }
+
+
+
 
 export default App;
